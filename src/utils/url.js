075cfe76aefc9f57d7bb7debde583237933001f7
Anthony Rossa <anthonysixty23@gmail.com>
2026-05-19 19:41:39 -0300

    change to url function

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === 'production'
+export const BASE_URL = import.meta.env.PROD
   ? 'https://fintrack-backend-fmsm.onrender.com'
   : 'http://localhost:3000';
