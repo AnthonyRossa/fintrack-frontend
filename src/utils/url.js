5956fc62e72fe7f9110abb2efc3165be06522eb8
Anthony Rossa <anthonysixty23@gmail.com>
2026-05-19 19:46:28 -0300

    change url function

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,4 @@
-export const BASE_URL = import.meta.env.PROD
-  ? 'https://fintrack-backend-fmsm.onrender.com'
-  : 'http://localhost:3000';
+export const BASE_URL = import.meta.env.VITE_API_URL
+  || (import.meta.env.PROD
+    ? 'https://fintrack-backend-fmsm.onrender.com'
+    : 'http://localhost:3000');
