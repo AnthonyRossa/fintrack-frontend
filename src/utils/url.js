5a377ee66c1500b9554a65699a644754099245e6
Anthony Rossa <anthonysixty23@gmail.com>
2026-05-19 20:08:15 -0300

    reinforced BaseUrl

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,5 @@
-export const BASE_URL = import.meta.env.VITE_API_URL
-  || (import.meta.env.PROD
-    ? 'https://fintrack-backend-fmsm.onrender.com'
-    : 'http://localhost:3000');
+const defaultBaseUrl = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:3000'
+  : 'https://fintrack-backend-fmsm.onrender.com';
+
+export const BASE_URL = import.meta.env.VITE_API_URL || defaultBaseUrl;
