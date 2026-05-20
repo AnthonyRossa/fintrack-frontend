4ddfeeb5f1378d3574cd8a4f9ad23d0632ecbbae
Anthony Rossa <anthonysixty23@gmail.com>
2026-05-20 19:21:34 -0300

    fix icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({ onLogout }) {
     return (
         <header className="header">
             <div className="header__title">
-            <img className='header__title-logo' src="./src/images/fticon.png" alt="FinTrack" />
+            <img className='header__title-logo' src="./src/images/fticon.svg" alt="FinTrack Icon" />
             <h1 className="header__title-text">FinTrack</h1>
             </div>
             {onLogout && (
