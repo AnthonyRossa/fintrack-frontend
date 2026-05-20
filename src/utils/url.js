9f86bca31d5f601af1c0778c7bb6335f2c908796
Anthony Rossa <anthonysixty23@gmail.com>
2026-05-19 21:12:50 -0300

    add env var

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,1 @@
-const defaultBaseUrl = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:3000'
-  : 'https://fintrack-backend-fmsm.onrender.com';
-
-export const BASE_URL = import.meta.env.VITE_API_URL || defaultBaseUrl;
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
